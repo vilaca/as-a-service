2e5e657912b52235c532d3df1bdcde3f623501c3
vilaca <jvilaca@gmail.com>
2023-06-26 22:49:56 +0100

    auto-sync

--- auto/deploy.sh
+++ auto/deploy.sh
@@ -7,20 +7,19 @@ export KUBECONFIG="$path"/kubeconfig
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-argocd cluster add single-node
+kubectl rollout status deploy/argocd-server --timeout=5m
+kubectl port-forward svc/argocd-server -n argocd 8080:443 &
+sleep 15
 
-kubectl port-forward svc/argocd-server -n argocd 8080:443&>2 &
 export argo_host=localhost:8080
 argocd admin initial-password -n argocd
 argocd login "$argo_host"
+argocd cluster add single-node
 
-kubectl create ns monitoring
-kubectl create ns dashboard
 kubectl config set-context --current --namespace=argocd
-
-argocd app create apps \
+argocd app create app-of-apps \
     --dest-namespace argocd \
     --dest-server https://kubernetes.default.svc \
+    --path argocd \
     --repo https://github.com/vilaca/as-a-service \
-    --path argocd
-argocd app sync apps  
+    --sync-policy auto
